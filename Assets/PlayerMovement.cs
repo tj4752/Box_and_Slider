9caf78749997ce29a5134d2584dbe90541f3b1a8
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-05 19:46:37 +0530

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
