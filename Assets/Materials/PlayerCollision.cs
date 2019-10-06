35fc57a6f599ebf2ca96cbbe50dc7ad1688d0fda
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-07 03:41:07 +0530

    scoring_left

---
+++ Assets/Materials/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+  public PlayerMovement movement;
+  private void OnCollisionEnter(Collision collisionInfo)
+  {
+    if (collisionInfo.collider.CompareTag("Obstacle"))
+      movement.enabled = false;
+    if(collisionInfo.collider.CompareTag("Ground"))
+      Debug.Log("We hit the Ground!!");
+  }
+}
