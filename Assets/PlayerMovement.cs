f79936e004f3bf30d0ee41dbc6eda3146f70125e
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-06 23:28:19 +0530

    Box_movement_done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.useGravity = false;
-    }
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0,0,forwardForce*Time.deltaTime);
+        if(Input.GetKey("d"))
+            rb.AddForce(sidewaysForce*Time.deltaTime, 0,0);
+        if(Input.GetKey("a"))
+            rb.AddForce(-sidewaysForce*Time.deltaTime, 0, 0);
     }
 }
